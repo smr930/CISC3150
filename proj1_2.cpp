bdb039f6c2d84fd637155b84a22fc219127294c4
Syed Raza <razasyedm@yahoo.com>
2016-03-25 13:59:44 -0400

    Update proj1_2.cpp

--- proj1_2.cpp
+++ proj1_2.cpp
@@ -1,44 +1,63 @@
 /*
- * [Store Credit Problem]
- * A test case consists of an integer C,
- * which is the store credit you receive, and a sequence of integers, which are prices of
- * the available items. The output for a test case consists of the indices, i and j (i < j),
- * of the two items whose prices add up to the store credit. It is assumed that each test
- * case will have exactly one solution.
+* [Store Credit Problem]
+* A test case consists of an integer C,
+* which is the store credit you receive, and a sequence of integers, which are prices of
+* the available items. The output for a test case consists of the indices, i and j (i < j),
+* of the two items whose prices add up to the store credit. It is assumed that each test
+* case will have exactly one solution.
 */
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
 
-void storeCredit(int arr[], int size, int credit)
-{
-	bool found = false;
-	for (int i = 0; i < size - 1; i++)
+int *storeCredit(std::vector<int> prices, int items, int credit){
+	int  temp[2];
+	for (int i = 0; i < items - 1; i++)
 	{
-		for (int j = 1; j < size; j++)
+		for (int j = i + 1; j < items; j++)
 		{
-			if (arr[i] + arr[j] == credit)
-			{
-				std::cout << arr[i] << " + " << arr[j] << " = " << credit
-					<< ", These values are at index " << i << " and " << j
-					<< std::endl;
-				found = true;
+			if (!(prices[i] + prices[j] == credit))
+				;
+			else {
+				temp[0] = i + 1, temp[1] = j + 1;
 			}
 		}
 	}
-	if (!found)
-	{
-		std::cout << "There were no two values that combine to the storecredit"
-			<< std::endl;
-	}
-
+	return temp;
 }
 
-int main()
+int main() 
 {
-	const int SIZE = 5;
-	int credit = 10;
-	int values[SIZE] = { 5, 2, 4, 8, 5 };
-	storeCredit(values, SIZE, credit);
+	std::ifstream infile("input.in");
+	if (!infile) 
+	{
+		std::cout << "could not open input file" << std::endl;
+		exit(EXIT_FAILURE);
+	}
+	std::ofstream outfile("output.out");
 
+	int num_cases, i = 0, items, credit;
+	infile >> num_cases;
+
+	for (int i = 0; i < num_cases; i++) 
+	{
+		infile >> credit;
+		infile >> items;
+
+		int price;
+		std::vector<int> prices;
+
+		for (int j = 0; j < items; j++) 
+		{
+			infile >> price;
+			prices.push_back(price);
+		}
+
+		int *val = storeCredit(prices, items, credit);
+		outfile << "Case #" << i + 1 << ": " << val[0] << " " << val[1] << std::endl;
+		
+	}
 	std::cin.get();
 }
